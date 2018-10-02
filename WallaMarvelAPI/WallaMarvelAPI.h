027a0ac6154732a258262e306f37cfa77c0c3ad4
Mario Chinchilla <>
2018-10-02 16:47:27 +0200

    [Project] Added new target WallaMarvelAPI in order to store here the API calls as well as the models, serializers and so on...

---
+++ WallaMarvelAPI/WallaMarvelAPI.h
@@ -0,0 +1,19 @@
+//
+//  WallaMarvelAPI.h
+//  WallaMarvelAPI
+//
+//  Created by Mario Plaza on 2/10/18.
+//  Copyright © 2018 Mario Plaza. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WallaMarvelAPI.
+FOUNDATION_EXPORT double WallaMarvelAPIVersionNumber;
+
+//! Project version string for WallaMarvelAPI.
+FOUNDATION_EXPORT const unsigned char WallaMarvelAPIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WallaMarvelAPI/PublicHeader.h>
+
+
