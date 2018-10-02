74a2e6d5defbdb60f70e14d9ede8c311589007f4
Mario Chinchilla <>
2018-10-02 12:48:40 +0200

    [Project] First commit. [Pods] Added and installed pods.

---
+++ WallaMarvel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WallaMarvel
+//
+//  Created by Mario Plaza on 2/10/18.
+//  Copyright © 2018 Mario Plaza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
